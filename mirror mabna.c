c6e446be8f0da3f9741f0da9f19439d6c0e41a5d
Kiasoh <kiarashs37@gmail.com>
2022-11-11 21:38:31 +0330

    feature2 added. feature2=mabnaC

--- mirror mabna.c
+++ mirror mabna.c
@@ -11,9 +11,19 @@ int mabna10(int n, int k)
 	}
 	return number;
 }
+int mabnaC(int n, int k)
+{
+	int number=0;
+	while(n!=0)
+	{
+		number=number*10 + n%k;
+		n/=k;
+	}
+	return number;
+}
 int main()
 {
 	int a, b, c, w;
 	scanf("%d%d%d", &a, &b,&c);
-	w=mabna10(a,b);
+	mabnaC(mabna10(a,b), c);
 }
