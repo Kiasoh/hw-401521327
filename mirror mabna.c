bdd026f2ddbfbf8053b9c602ba099f89d699e2bf
Kiasoh <kiarashs37@gmail.com>
2022-11-11 21:34:04 +0330

    Feature1 added. feature1=mabna10

--- mirror mabna.c
+++ mirror mabna.c
@@ -1,7 +1,19 @@
 #include <stdio.h>
+#include <math.h>
+int mabna10(int n, int k)
+{
+	int counter10=0, number=0;
+	while(n!=0)
+	{
+		number+=(pow(k,counter10)*(n%10));
+		n/=10;
+		counter10++;
+	}
+	return number;
+}
 int main()
 {
-	int a, b, c;
+	int a, b, c, w;
 	scanf("%d%d%d", &a, &b,&c);
-	
+	w=mabna10(a,b);
 }
